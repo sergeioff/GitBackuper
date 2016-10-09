3b77f0e8ff68d15b22c4776d45a9d3804125f420
SergeiOFF <pogorelov.s.y@gmail.com>
2016-10-09 17:05:25 +0300

    http request fail test

--- src/test/java/gitBackuper/utils/HttpClientTest.java
+++ src/test/java/gitBackuper/utils/HttpClientTest.java
@@ -1,10 +1,27 @@
 package gitBackuper.utils;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class HttpClientTest {
+    private final ByteArrayOutputStream errStream = new ByteArrayOutputStream();
+    @Before
+    public void setErrStream() {
+        System.setErr(new PrintStream(errStream));
+    }
+
     @Test
     public void testHttpRequest() {
-        System.out.println(HttpClient.makeRequest("https://api.github.com"));
+        Assert.assertTrue(HttpClient.makeRequest("https://api.github.com").length() > 0);
+    }
+
+    @Test
+    public void httpRequestWillFail() {
+        HttpClient.makeRequest("asfasfasf");
+        Assert.assertTrue(errStream.toString().contains("Failed to get"));
     }
 }
