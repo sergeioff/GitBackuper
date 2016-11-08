de5a2a4ab38a3e9d1a1d74d3db72b16e5e2129ae
SergeiOFF <pogorelov.s.y@gmail.com>
2016-11-08 20:53:05 +0200

    added JavaDoc & functionality

--- src/main/java/gitBackuper/api/GitHub.java
+++ src/main/java/gitBackuper/api/GitHub.java
@@ -14,21 +14,18 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Provides access to GitHub API.
+ */
 public class GitHub {
-    private final String API_ROOT = "https://api.github.com/";
-    private final String USER_PATTERN = API_ROOT + "users/%s";
-    private final String REPOS_FOR_USER_PATTERN = USER_PATTERN + "/repos";
-    private final String REPOSITORY_PATTERN = API_ROOT + "repos/%s/%s";
-    private final String CONTENTS_PATTERN = REPOSITORY_PATTERN + "/contents/";
-    private final String REQUEST_WITH_ACCESS_TOKEN = "%s?access_token=%s";
-    private final String CURRENT_USER = API_ROOT + "user";
+    private ObjectMapper mapper;
+    private UrlCreationHelper urlCreationHelper;
 
-    ObjectMapper mapper;
-
-    private String token = null;
+    private String token;
 
     public GitHub() {
         mapper = new ObjectMapper();
+        urlCreationHelper = new UrlCreationHelper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
@@ -37,83 +34,79 @@ public class GitHub {
         this.token = token;
     }
 
+    /**
+     * Get current logged user (via token)
+     * @return current logged user
+     * @throws IOException if there are problems with url connection
+     */
     public User getCurrentUser() throws IOException {
         if (token == null) {
             throw new IllegalStateException();
         }
 
-        String url = getLinkForRequestWitchToken(CURRENT_USER, token);
+        String url = urlCreationHelper.getLinkForRequestWitchToken(urlCreationHelper.getLinkForCurrentUser(), token);
 
         return mapper.readValue(new URL(url), User.class);
     }
 
-    public String getLinkForUser(String username) {
-        String link = String.format(USER_PATTERN, username);
-        if (null == token) {
-            return link;
-        }
-
-        return getLinkForRequestWitchToken(link, token);
+    /**
+     * Get user from GitHub by username
+     * @param username username
+     * @return user
+     * @throws IOException if there are problems with url connection
+     */
+    public User getUser(String username) throws IOException {
+        String linkForUser = urlCreationHelper.getLinkForUser(username);
+        return mapper.readValue(new URL(linkForUser), User.class);
     }
 
-    public String getLinkForUserRepos(String username) {
-        String link = String.format(REPOS_FOR_USER_PATTERN, username);
+    /**
+     * Get repository from GitHub by username and repository name
+     * @param user repository owner
+     * @param repository repository name
+     * @return repository
+     * @throws IOException if there are problems with url connection
+     */
+    public Repository getRepository(String user, String repository) throws IOException {
+        String linkForRepository = urlCreationHelper.getLinkForRepository(user, repository);
+        Repository repo = mapper.readValue(new URL(linkForRepository), Repository.class);
+        repo.setContentFiles(getRepositoryContentFiles(user, repository));
 
-        if (null == token) {
-            return link;
-        }
-
-        return getLinkForRequestWitchToken(link, token);
+        return repo;
     }
 
-    public String getLinkForRepository(String url) {
-        Pattern pattern = Pattern.compile("github.com/([a-zA-Z0-9-]+)/([a-zA-Z0-9-]+)");
-        Matcher matcher = pattern.matcher(url);
+    /**
+     * Get repository from GitHub by url
+     * @param url repository url
+     * @return repository
+     * @throws IOException if there are problems with url connection
+     */
+    public Repository getRepository(String url) throws IOException {
+        UrlCreationHelper.RepositoryAndOwner parsingResult = urlCreationHelper.getRepositoryAndOwner(url);
 
-        String username;
-        String repository;
-
-        if (matcher.find()) {
-            username = matcher.group(1);
-            repository = matcher.group(2);
-        } else {
-            throw new IllegalArgumentException();
-        }
-
-        return getLinkForRepository(username, repository);
+        return getRepository(parsingResult.ownerName, parsingResult.repositoryName);
     }
 
-    public String getLinkForRepository(String user, String repository) {
-        String link = String.format(REPOSITORY_PATTERN, user, repository);
-
-        if (null == token) {
-            return link;
-        }
-
-        return getLinkForRequestWitchToken(link, token);
-    }
-
-    public String getLinkForRepositoryContents(String user, String repository) {
-        String link = String.format(CONTENTS_PATTERN, user, repository);
-
-        if (null == token) {
-            return link;
-        }
-
-        return getLinkForRequestWitchToken(link, token);
-    }
-
-    public String getLinkForRequestWitchToken(String request, String token) {
-        return String.format(REQUEST_WITH_ACCESS_TOKEN, request, token);
-    }
-
-    public List<ContentFile> getRepositoryContentFiles(String user, String repository) throws IOException {
-        String linkForContents = getLinkForRepositoryContents(user, repository);
+    /**
+     * Get content files for repository
+     * @param user repository owner username
+     * @param repository repository name
+     * @return content files
+     * @throws IOException if there are problems with url connection
+     */
+    private List<ContentFile> getRepositoryContentFiles(String user, String repository) throws IOException {
+        String linkForContents = urlCreationHelper.getLinkForRepositoryContents(user, repository);
 
         return getDirectoryContentFiles(linkForContents);
     }
 
-    public List<ContentFile> getDirectoryContentFiles(String url) throws IOException {
+    /**
+     * Get content files from specified directory
+     * @param url url to directory
+     * @return content files from directory
+     * @throws IOException if there are problems with url connection
+     */
+    private List<ContentFile> getDirectoryContentFiles(String url) throws IOException {
         List<ContentFile> filesInDirectory = mapper.readValue(new URL(url),
                 mapper.getTypeFactory().constructCollectionType(List.class, ContentFile.class));
 
@@ -136,16 +129,150 @@ public class GitHub {
         return filesInDirectory;
     }
 
-    public User getUser(String username) throws IOException {
-        String linkForUser = getLinkForUser(username);
-        return mapper.readValue(new URL(linkForUser), User.class);
-    }
+    /**
+     * Used to creating links to GitHub API
+     */
+    private class UrlCreationHelper {
+        private final String API_ROOT = "https://api.github.com/";
+        private final String USER_PATTERN = API_ROOT + "users/%s";
+        private final String REPOS_FOR_USER_PATTERN = USER_PATTERN + "/repos";
+        private final String REPOSITORY_PATTERN = API_ROOT + "repos/%s/%s";
+        private final String CONTENTS_PATTERN = REPOSITORY_PATTERN + "/contents/";
+        private final String REQUEST_WITH_ACCESS_TOKEN = "%s?access_token=%s";
+        private final String CURRENT_USER = API_ROOT + "user";
 
-    public Repository getRepository(String user, String repository) throws IOException {
-        String linkForRepository = getLinkForRepository(user, repository);
-        Repository repo = mapper.readValue(new URL(linkForRepository), Repository.class);
-        repo.setContentFiles(getRepositoryContentFiles(user, repository));
+        /**
+         * Makes url to request user via GitHub API
+         * @param username username
+         * @return url for request
+         */
+        private String getLinkForUser(String username) {
+            String link = String.format(USER_PATTERN, username);
+            if (null == token) {
+                return link;
+            }
 
-        return repo;
+            return getLinkForRequestWitchToken(link, token);
+        }
+
+        /**
+         * Makes url to request current logged user via GitHub API
+         * @return url for request
+         */
+        private String getLinkForCurrentUser() {
+            return CURRENT_USER;
+        }
+
+        /**
+         * Makes url to request user repositories via GitHub API
+         * @param username username
+         * @return url for request
+         */
+        private String getLinkForUserRepos(String username) {
+            String link = String.format(REPOS_FOR_USER_PATTERN, username);
+
+            if (null == token) {
+                return link;
+            }
+
+            return getLinkForRequestWitchToken(link, token);
+        }
+
+        /**
+         * Makes url to request repository via GitHub API
+         * @param url url of repository
+         * @return url for request
+         */
+        private String getLinkForRepository(String url) {
+            RepositoryAndOwner parsingResult = getRepositoryAndOwner(url);
+            return getLinkForRepository(parsingResult.ownerName, parsingResult.repositoryName);
+        }
+
+        /**
+         * Makes url to request repository via GitHub API
+         * @param user repository owner
+         * @param repository repository name
+         * @return url for request
+         */
+        private String getLinkForRepository(String user, String repository) {
+            String link = String.format(REPOSITORY_PATTERN, user, repository);
+
+            if (null == token) {
+                return link;
+            }
+
+            return getLinkForRequestWitchToken(link, token);
+        }
+
+        /**
+         * Makes url to request repository via GitHub API
+         * @param url url of repository
+         * @return url for request
+         */
+        private String getLinkForRepositoryContents(String url) {
+            RepositoryAndOwner parsingResult = getRepositoryAndOwner(url);
+            return getLinkForRepositoryContents(parsingResult.ownerName, parsingResult.repositoryName);
+        }
+
+        /**
+         * Makes url to request repository contents via GitHub API
+         * @param user repository owner
+         * @param repository repository name
+         * @return url for request
+         */
+        private String getLinkForRepositoryContents(String user, String repository) {
+            String link = String.format(CONTENTS_PATTERN, user, repository);
+
+            if (null == token) {
+                return link;
+            }
+
+            return getLinkForRequestWitchToken(link, token);
+        }
+
+        /**
+         * Adds OAuth token to request
+         * @param request initial request
+         * @param token OAuth token
+         * @return authorized url
+         */
+        private String getLinkForRequestWitchToken(String request, String token) {
+            return String.format(REQUEST_WITH_ACCESS_TOKEN, request, token);
+        }
+
+        /**
+         * Parses username and repository name from url
+         * @param url url to parse
+         * @return result of parsing
+         */
+        private RepositoryAndOwner getRepositoryAndOwner(String url) {
+            Pattern pattern = Pattern.compile("github.com/([a-zA-Z0-9-]+)/([a-zA-Z0-9-]+)");
+            Matcher matcher = pattern.matcher(url);
+
+            String username;
+            String repository;
+
+            if (matcher.find()) {
+                username = matcher.group(1);
+                repository = matcher.group(2);
+            } else {
+                throw new IllegalArgumentException();
+            }
+
+            return new RepositoryAndOwner(username, repository);
+        }
+
+        /**
+         * Used for parsing urls to username and repository
+         */
+        private class RepositoryAndOwner {
+            String repositoryName;
+            String ownerName;
+
+            RepositoryAndOwner(String repositoryName, String ownerName) {
+                this.repositoryName = repositoryName;
+                this.ownerName = ownerName;
+            }
+        }
     }
 }
